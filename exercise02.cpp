5a3c11cbd15b1587b09a116b8119b70e43f33cc0
Godamunna D.R.B.N.B <99312319+It21213380@users.noreply.github.com>
2022-03-01 04:40:05 +0000

    complete 2

--- exercise02.cpp
+++ exercise02.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-using namespace std;
 
 int volume(int height, int width, int length);
 
@@ -8,26 +7,35 @@ int main() {
     int box2Height, box2Width, box2Length;
     int totalVolume, totalSurface;
     
-    cout << "Enter Box 1 Height : ";
-    cin >> box1Height;
-    cout << "Enter Box 1 Width : ";
-    cin >> box1Width;
-    cout << "Enter Box 1 Length : ";
-    cin >> box1Length;
+    std::cout << "Enter Box 1 Height : ";
+    std::cin >> box1Height;
+    std::cout << "Enter Box 1 Width : ";
+    std::cin >> box1Width;
+    std::cout << "Enter Box 1 Length : ";
+    std::cin >> box1Length;
     
-     cout << "Enter Box 2 Height : ";
-    cin >> box2Height;
-    cout << "Enter Box 2 Width : ";
-    cin >> box2Width;
-    cout << "Enter Box 2 Length : ";
-    cin >> box2Length;
+    std::cout << "Enter Box 2 Height : ";
+    std::cin >> box2Height;
+    std::cout << "Enter Box 2 Width : ";
+    std::cin >> box2Width;
+    std::cout << "Enter Box 2 Length : ";
+    std::cin >> box2Length;
     
     totalVolume = volume(box1Height, box1Width, box1Length)
              + volume(box2Height, box2Width, box2Length);
              
-    cout << "Volume of Box is " << totalVolume << endl;
+    std::cout << "Volume of Boxes is " << totalVolume << std::endl;
     
     return 0;
 }
 
 // Implement the Volume() function here
+int volume(int height, int width, int length)
+{
+   int vol = 0;
+
+   vol = height * width * length;
+
+   return vol;
+}
+
